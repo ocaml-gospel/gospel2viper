4bd73147d3a75c74fd528e821c68d1c52046110f
Lucccyo <cha.git@mailo.fr>
2024-07-10 12:53:27 +0200

    add expr for method body + some adjustements in the whole ast

--- lib/print_viper.ml
+++ lib/print_viper.ml
@@ -21,13 +21,21 @@ let implies = string "==>" ^^ break 1
 let crlet = string "let "
 let spaceeqspace = string " == "
 let spaceinspace = string " in "
+let notspace = string "not "
 let ifspace = string "if "
 let spaceelsespace = string " else "
 let spaceintmarkspace = string " ? "
 let spacecolonspace = string " : "
 let not = string "!"
+let minus = string "!"
+let varspace = string "var "
 let spaceandandspace = string " && "
+let ttrue = string "true"
+let null = string "null"
+let ffalse = string "false"
 let reqspace = string "requires "
+let nnew = string "new"
+let spaceassignspace = string " := "
 let ensspace = string "ensures "
 let methspace = string "method "
 let funspace = string "function "
@@ -39,10 +47,10 @@ let spacereturnsspace = break 1 ^^ string "returns "
 let block n opening contents closing =
   group (opening ^^ nest n contents ^^ closing)
 let block = block indentation
-let parens d   = block lparen   (break 0 ^^ d) (break 0 ^^ rparen)
+let parens   d = block lparen   (break 0 ^^ d) (break 0 ^^ rparen)
 let brackets d = block lbracket (break 0 ^^ d) (break 0 ^^ rbracket)
-let braces d   = block lbrace   (break 1 ^^ d) (break 1 ^^ rbrace)
-let pipes d    = (string "|") ^^ d ^^ (string "|")
+let braces   d = block lbrace   (break 1 ^^ d) (break 1 ^^ rbrace)
+let pipes    d = (string "|") ^^ d ^^ (string "|")
 
 (* Printers for Viper's AST *)
 let rec pp_list pp_elem = function
@@ -50,48 +58,88 @@ let rec pp_list pp_elem = function
   | elem :: [] -> pp_elem elem
   | elem :: tl -> pp_elem elem ^^ commaspace ^^ pp_list pp_elem tl
 
-let pp_op = function
+let pp_binop = function
   | BAnd  -> string "&&"
   | BOr   -> string "||"
   | BImpl -> string "==>"
+
+let pp_const = function
+  | CInt n -> string (string_of_int n)
 
 let rec pp_ty = function
   | TyApp (s, tys) ->
     string s ^^ (if tys == [] then empty else brackets (pp_list pp_ty tys))
   | TyVar s -> string s
 and pp_term = function
-  | TConst n -> string (string_of_int n)
-  | TBool  b -> if b then string "true" else string "false"
-  | TNull -> string "null"
+  | TConst n -> pp_const n
+  | TBool  b -> if b then ttrue else ffalse
+  | TNull -> null
   | TApp (_ty_opt, s, terms) ->
     group (string s ^^ parens (pp_list pp_term terms))
-  | TVar (prefix, s) ->
-    (match prefix with
-    | None -> empty
-    | Some n -> pp_term n ^^ dot) ^^ string s
+  | TVar s -> string s
+  | TField (term, field) -> (* TODO : parens if term <> TVar *)
+    pp_term term ^^ dot ^^ string field
   | TInfix (term1, symb, term2) ->
     parens (pp_term term1 ^^ space ^^ string symb ^^ space ^^ pp_term term2)
   | TSeq seq -> pp_tseq seq
-  | TBinop (t1, symb, t2) -> parens (pp_term t1 ^^ space ^^ pp_op symb ^^ space ^^ pp_term t2)
-  | TNot term -> not ^^ pp_term term
-  | TAcc (model, field) -> acc ^^ parens (string model ^^ dot ^^ string field)
+  | TBinop (t1, symb, t2) ->
+    parens (pp_term t1 ^^ space ^^ pp_binop symb ^^ space ^^ pp_term t2)
+  | TNeg term -> minus ^^ pp_term term
+  | TAcc (term, field) -> (* TODO : parens if term <> TVar *)
+    acc ^^ parens (pp_term term ^^ dot ^^ string field)
   | TLet (name, t1, t2) ->
-    crlet ^^ string name ^^ spaceeqspace ^^ parens (pp_term t1) ^^ spaceinspace ^^ pp_term t2
-  | TIf (tif, tthen, telse_opt) -> ifspace ^^ parens (pp_term tif) ^^ space ^^ braces (pp_term tthen) ^^
-    (match telse_opt with
-    | None -> empty
-    | Some telse -> spaceelsespace ^^ braces (pp_term telse))
-  | TTernary (tif, tthen, telse) -> parens (pp_term tif ^^ spaceintmarkspace ^^ pp_term tthen ^^ spacecolonspace ^^ pp_term telse)
+    crlet ^^ string name ^^ spaceeqspace ^^
+    parens (pp_term t1) ^^ spaceinspace ^^ pp_term t2
+  | TTernary (tif, tthen, telse) ->
+    parens (pp_term tif ^^ spaceintmarkspace ^^
+    pp_term tthen ^^ spacecolonspace ^^ pp_term telse)
+  | TNot term -> notspace ^^ parens (pp_term term)
 and pp_tseq = function
   | TEmpty ty -> pp_ty (TyApp("Seq", [ty])) ^^ parens empty
   | TSingleton term -> string "Seq" ^^ parens (pp_term term)
-  | TConcat (term1, term2) -> pp_term term1 ^^ spaceconcatspace ^^ pp_term term2
+  | TConcat (term1, term2) ->
+    pp_term term1 ^^ spaceconcatspace ^^ pp_term term2
   | TLength term -> pipes (pp_term term)
   | TGet (s, term) -> (pp_term s) ^^ brackets (pp_term term)
   | TSub (s, term1, term2_opt) ->
     pp_term s ^^ brackets (pp_term term1 ^^ dotdot ^^ (
     match term2_opt with
     | Some term2 -> pp_term term2
+    | None -> empty))
+
+let rec pp_expr = function
+  | EConst c -> pp_const c
+  | EBool b -> if b then ttrue else ffalse
+  | ENull -> null
+  | EApp (name, exprs) -> string name ^^ parens (pp_list pp_expr exprs)
+  | EVariable name -> string name
+  | ENeg expr -> minus ^^ pp_expr expr
+  | EBinop (e1, binop, e2) ->
+    parens (pp_expr e1 ^^ space ^^ pp_binop binop ^^ space ^^ pp_expr e2)
+  | ESeq seq -> pp_eseq seq
+  | EInfix (e1, infix, e2) ->
+    parens (pp_expr e1 ^^ space ^^ string infix ^^ space ^^ pp_expr e2)
+  | EIf (tif, tthen, telse_opt) ->
+    ifspace ^^ parens (pp_expr tif) ^^ space ^^ braces (pp_expr tthen) ^^
+    (match telse_opt with
+    | None -> empty
+    | Some telse -> spaceelsespace ^^ braces (pp_expr telse))
+  | EField (expr, field) -> (* TODO : parens if term <> TVar *)
+    pp_expr expr ^^ dot ^^ string field
+  | ENew (exprs) -> nnew ^^ parens (pp_list pp_expr exprs)
+  | EAssig (e1, e2) -> pp_expr e1 ^^ spaceassignspace ^^ pp_expr e2
+  | EVar (lbl, ty)  -> varspace ^^ string lbl ^^ spacecolonspace ^^ pp_ty ty
+and pp_eseq = function
+  | EEmpty ty -> pp_ty (TyApp("Seq", [ty])) ^^ parens empty
+  | ESingleton expr -> string "Seq" ^^ parens (pp_expr expr)
+  | EConcat (expr1, expr2) ->
+    pp_expr expr1 ^^ spaceconcatspace ^^ pp_expr expr2
+  | ELength expr -> pipes (pp_expr expr)
+  | EGet (s, expr) -> (pp_expr s) ^^ brackets (pp_expr expr)
+  | ESub (s, expr1, expr2_opt) ->
+    pp_expr s ^^ brackets (pp_expr expr1 ^^ dotdot ^^ (
+    match expr2_opt with
+    | Some expr2 -> pp_expr expr2
     | None -> empty))
 
 let pp_cond prefix conds =
@@ -105,11 +153,13 @@ let pp_spec (spec: spec) = match spec.spec_pre, spec.spec_post with
   | [], [] -> empty
   | [], post  -> group (pp_cond ensspace post)
   | pre, []   -> group (pp_cond reqspace pre)
-  | pre, post -> group (pp_cond reqspace pre) ^^ break 1 ^^ group (pp_cond ensspace post)
+  | pre, post ->
+    group (pp_cond reqspace pre) ^^ break 1 ^^ group (pp_cond ensspace post)
 
 let rec pp_val_type_list = function
   | [] -> empty
-  | (arg_name, arg_type) :: [] -> string arg_name ^^ colonspace ^^ pp_ty arg_type
+  | (arg_name, arg_type) :: []  ->
+    string arg_name ^^ colonspace ^^ pp_ty arg_type
   | (arg_name, arg_type) :: tl  ->
     string arg_name ^^ colonspace ^^ pp_ty arg_type ^^
     (if tl = [] then empty else commaspace) ^^ pp_val_type_list tl
@@ -117,22 +167,36 @@ let rec pp_val_type_list = function
 let pp_args args = parens (pp_val_type_list args)
 
 let pp_returns returns =
-  if returns = [] then empty else spacereturnsspace ^^ parens (pp_val_type_list returns)
+  if returns = [] then empty
+  else spacereturnsspace ^^ parens (pp_val_type_list returns)
+
+let pp_expr_body = function
+  | None -> empty
+  | Some el -> braces (
+    let rec iter = function
+    | [] -> empty
+    | elem :: [] -> pp_expr elem
+    | elem :: tl -> pp_expr elem ^^ hardline ^^ iter tl in
+    iter el
+    )
 
 let pp_method_def m =
-  prefix 2 1
-    (group (methspace ^^ string m.method_name ^^ pp_args m.method_args ^^ pp_returns m.method_returns))
-    (pp_spec m.method_spec)
+    methspace ^^ string m.method_name ^^ pp_args m.method_args ^^
+    pp_returns m.method_returns ^^
+    hardline ^^ pp_spec m.method_spec ^^ pp_expr_body m.method_body
 
-let pp_body = function
+let pp_term_body = function
   | None   -> empty
   | Some t -> braces (pp_term t)
 
 let pp_predicate_def p =
-  predspace ^^ string p.pred_name ^^ pp_args p.pred_args ^^ space ^^ pp_body p.pred_body
+  predspace ^^ string p.pred_name ^^ pp_args p.pred_args ^^
+  space ^^ pp_term_body p.pred_body
 
 let pp_function_def f =
-  funspace ^^ string f.function_name ^^ pp_args f.function_args ^^ colonspace ^^ pp_ty f.function_rety ^^ space ^^ pp_spec f.function_spec ^^ space ^^ pp_body f.function_body
+  funspace ^^ string f.function_name ^^ pp_args f.function_args ^^
+  colonspace ^^ pp_ty f.function_rety ^^ space ^^
+  pp_spec f.function_spec ^^ space ^^ pp_term_body f.function_body
 
 let pp_decl = function
   | DPredicate pred_def -> pp_predicate_def pred_def
--- lib/viper_ast.ml
+++ lib/viper_ast.ml
@@ -1,29 +1,86 @@
 type binop = BAnd | BOr | BImpl
 
+type label = string
+
 type ty =
-  | TyApp of string * (ty list)
-  | TyVar of string
+  | TyApp of label * (ty list)
+  | TyVar of label
+
+type const = CInt of int
+
 and term =
-  | TConst of int
+  | TConst of const
   | TBool of bool
   | TNull
-  | TApp of ty option * string * term list
-  | TVar of term option * string
+    (* null keyword for null references *)
+  | TApp of ty option * label * term list
+    (* f(t1, t2, ...) *)
+  | TVar of label
+  | TField of term * label
+    (* t1.t2 *)
   | TInfix of term * string * term
   | TSeq of tseq
   | TBinop of term * binop * term
-  | TNot of term
-  | TAcc of string * string
-  | TLet of string * term * term
-  | TIf of term * term * term option
+  | TNeg of term
+    (* - t *)
+  | TAcc of term * label
+    (* acc(t.field) *)
+  | TLet of label * term * term
+    (* let f = t1 in t2 *)
   | TTernary of term * term * term
+    (* bterm ? t1 : t2 *)
+  | TNot of term
+    (* not t *)
 and tseq =
   | TEmpty of ty
+    (* Seq[Int]() *)
   | TSingleton of term
+    (* Seq(t) *)
   | TConcat of term * term
   | TLength of term
+    (* |seq| *)
   | TGet of term * term
+    (* [n] *)
   | TSub of term * term * term option
+    (* [_.._] *)
+
+and expr =
+  | EConst of const
+  | EBool of bool
+  | ENull
+    (* null keyword for null references *)
+  | EApp of label * (expr list)
+    (* f(e1, e2, ...) *)
+  | EVariable of label
+  | ENeg of expr
+    (* - t *)
+  | EBinop of expr * binop * expr
+  | ESeq of eseq
+  | EInfix of expr * string * expr
+  | EIf of expr * expr * expr option
+    (* if (bexpr) {e1} else {e2}
+       if (bexpr) {e1}
+    *)
+  | EVar of label * ty
+    (* var t : Int *)
+  | EField of expr * label
+    (* t.e1 *)
+  | ENew of (expr list)
+    (* new(e1, e2, ...) *)
+  | EAssig of expr * expr
+    (* t1 := t2 *)
+and eseq =
+  | EEmpty of ty
+    (* Seq[Int]() *)
+  | ESingleton of expr
+    (* Seq(t) *)
+  | EConcat of expr * expr
+  | ELength of expr
+    (* |seq| *)
+  | EGet of expr * expr
+    (* [n] *)
+  | ESub of expr * expr * expr option
+    (* [_.._] *)
 
 type spec = {
   spec_pre:  term list;
@@ -31,21 +88,22 @@ type spec = {
 }
 
 type predicate_def = {
-  pred_name: string;
+  pred_name: label;
   pred_body: term option;
-  pred_args: (string * ty) list;
+  pred_args: (label * ty) list;
 }
 
 type method_def = {
-  method_name: string;
-  method_args: (string * ty) list;
-  method_returns: (string * ty) list;
+  method_name: label;
+  method_args: (label * ty) list;
+  method_returns: (label * ty) list;
   method_spec: spec;
+  method_body: (expr list) option;
 }
 
 type function_def = {
-  function_name: string;
-  function_args: (string * ty) list;
+  function_name: label;
+  function_args: (label * ty) list;
   function_rety: ty;
   function_spec: spec;
   function_body: term option;
@@ -55,7 +113,7 @@ type decl =
   | DPredicate of predicate_def
   | DMethod of method_def
   | DFunction of function_def
-  | DField of string * ty
+  | DField of label * ty
   | DBlank
 
 type program = decl list
